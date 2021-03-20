96f9866da275e54465da50584a9d0fb62589db1b
Ryota Hirai <g1853806@cc.kyoto-su.ac.jp>
2021-03-20 22:41:16 +0900

    Made a form

--- app/controllers/presentation_controller.rb
+++ app/controllers/presentation_controller.rb
@@ -7,8 +7,4 @@ class PresentationController < ApplicationController
 
   def new
   end
-
-  #スライドと声を登録する画面で「Create Presentation」を押すと、ここに飛ぶ
-  def create
-  end
 end
--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -7,4 +7,13 @@ class PresentationsController < ApplicationController
 
   def new
   end
+
+  #スライドと声を登録する画面で「Create Presentation」を押すと、ここに飛ぶ
+  def creates
+    @presentation = Presentation.new
+    @presentation.title = params[:presentation][:title]
+    @presentation.content = params[:presentation][:content]
+    @presentation.save
+    redirect_to '/presentations/index'
+  end
 end
